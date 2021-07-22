d72425c767f87dcc01efb2d5d1b8c2ee28e26ca0
Tiberiu Georgescu <tiberiu.georgescu@nutanix.com>
2021-07-22 13:51:56 +0000

    Add script to (re)set 'examples' cgroup on sys

---
+++ reset_test_cgroup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SWAPPINESS=60
+LIMIT_IN_BYTES=1M
+
+while getopts ":s:l:" arg; do
+  case $arg in
+    s) SWAPPINESS=$OPTARG;;
+    l) LIMIT_IN_BYTES=$OPTARG;;
+  esac
+done
+
+cgdelete -g memory:examples
+cgcreate -g memory:examples
+cgset -r memory.swappiness=$SWAPPINESS examples
+cgset -r memory.limit_in_bytes=$LIMIT_IN_BYTES examples
+
+cgget -r memory.swappiness -r memory.limit_in_bytes -r memory.usage_in_bytes examples
