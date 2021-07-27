d36a82c823cf1834339eacad37a6bc460ba0afcf
tiberiu-georgescu <tiberiu.georgescu@nutanix.com>
2021-07-27 08:05:15 +0000

    [REFACTOR] add comments & general logic refactor for dd_pagemap

--- dd_pagemap.sh
+++ dd_pagemap.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-OPTIND=1
-
-OFFSET=0
+# Default Parameters
+VADDR=$((0x600000000000))
 COUNT=256
+OFFSET=0
+ITERATIONS=10
+BATCH_SIZE=1
 
-while getopts ":p:v:c:o:" arg; do
+# Initialising Parameters
+OPTIND=1
+while getopts ":p:v:c:o:b:" arg; do
   case $arg in
     p) PID=$OPTARG;;
     v) VADDR=$OPTARG;;
     c) COUNT=$OPTARG;;
     o) OFFSET=$OPTARG;;
+    b) BATCH_SIZE=$OPTARG;;
+    ?) echo "Invalid option: -${OPTARG}."
+       exit 1 ;;
   esac
 done
 
-dd if=/proc/$PID/pagemap ibs=8 skip=$(($VADDR / 4096 + $OFFSET)) count=$COUNT 2>/dev/null
+# Convert Params for Batching
+IBS=$(($BATCH_SIZE * 8))
+BATCH_SKIP=$((($VADDR / 4096 + $OFFSET) / $BATCH_SIZE))
+BATCH_COUNT=$(($COUNT / $BATCH_SIZE))
+
+# Run program once
+dd if=/proc/$PID/pagemap ibs=$IBS skip=$BATCH_SKIP count=$BATCH_COUNT 2>/dev/null
 
 OPTIND=1
